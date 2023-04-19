1a893e213729bc6bf2466d2bd07e9b721d4724b8
Christian Weck <weck.chr@gmail.com>
2023-04-19 09:42:32 +0200

    ScheduleUpdate

--- src/main/java/LambdaStream.java
+++ src/main/java/LambdaStream.java
@@ -3,8 +3,8 @@ public class LambdaStream {
     //Link auf Mitschrift: https://github.com/FullOfPit/hh-java-23-1-lambdastream-demo
     //Link auf Übungsaufgaben: https://github.com/neuefische/hh-java-23-1-lambdastreams-exercise
 
-    //1) Übungsaufgaben bis -- xx.xx
-    //2) 5 min Pause xx.xx - xx.xx
+    //1) Übungsaufgaben bis -- 10.40
+    //2) 5 min Pause 10.40 - 10.45
     //3) Vorstellung Lambdas
     //4) 15 - 30 min Aufgabe bis xx.xx
     //5) Vorstellung Streams
